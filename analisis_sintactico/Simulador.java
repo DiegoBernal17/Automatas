704b2a53a11d870b642cf3d20b697a8116606c94
Diego Bernal <diego.bernal17@gmail.com>
2017-11-18 12:15:06 -0600

    Comienzo analisis sintactico

---
+++ analisis_sintactico/Simulador.java
@@ -0,0 +1,32 @@
+package analisis_sintactico;
+
+public class Simulador {
+    private String cadena_original;
+    private String cadena_actual;
+    private char caracter_actual;
+
+    public void variables() {
+
+    }
+
+    public void ciclos() {
+
+    }
+
+    public void condiciones() {
+
+    }
+
+    public void avanzar() {
+
+    }
+
+    public void inicar() {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
